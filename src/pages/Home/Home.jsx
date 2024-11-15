5fed2d1f2b427321b957bcda32a1c94482dc2348
Gobinda B <gobinda2001b@gmail.com>
2024-11-15 10:44:32 +0530

    home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
-<<<<<<< HEAD
 import HeroSection from './HeroSection'
 import MarginWrapper from '../../common/MarginWrapper'
 import NewArrivals from './NewArrivals'
 import ExploreCollection from './ExploreCollection'
 
-=======
-import FeaturedProduct from './FeaturedProduct'
->>>>>>> main
 
 const Home = () => {
   return (

889de901f2e66be057909015274eaab25d226c96
Gobinda B <gobinda2001b@gmail.com>
2024-11-29 10:27:31 +0530

    test

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,24 +1,30 @@
-import React from "react";
-import HeroSection from "./components/HeroSection";
-import MarginWrapper from "../../common/MarginWrapper";
-import NewArrivals from "./components/NewArrivals";
-import ExploreCollection from "./components/ExploreCollection";
-import FeatureProduct from "./components/FeaturedProduct";
-import MySiteMission from "./components/MySiteMission";
-import WeekendSale from "./components/WeekendSale";
-import Testimonial from "./components/Testimonial";
+import React from 'react'
+import HeroSection from './components/HeroSection'
+import MarginWrapper from '../../common/MarginWrapper'
+import NewArrivals from './components/NewArrivals'
+import ExploreCollection from './components/ExploreCollection'
+import FeatureProduct from './components/FeaturedProduct'
+import MySiteMission from './components/MySiteMission'
+import WeekendSale from './components/WeekendSale'
+import Testimonial from './components/Testimonial'
 const Home = () => {
   return (
     <div>
-      <HeroSection />
-      <NewArrivals />
-      {/* <ExploreCollection />
+    <HeroSection />
+    <MarginWrapper >
+    <NewArrivals />
+    </MarginWrapper>
+     <MarginWrapper >
+      <ExploreCollection/>
+     </MarginWrapper>
       <FeatureProduct />
-      <MySiteMission />
-      <WeekendSale />
-      <Testimonial /> */}
+     <MySiteMission />
+      <MarginWrapper >
+        <WeekendSale/>
+      </MarginWrapper>
+      <Testimonial />
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
